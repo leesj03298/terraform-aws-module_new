2e7d9dfaa4775622e3b382ffc281e0cb45c29126
lsj <leesj03298@naver.com>
2024-04-26 14:41:21 +0900

    route module update

--- route/data.tf
+++ route/data.tf
@@ -1,27 +1,27 @@
 locals {
   create_route = length(var.routes) > 0 ? true : false
+  route_table_name_optimize = distinct([for route in var.routes : route.route_table_name])
+  internet_gateway_optimize = distinct([for route in var.routes : route.target_name if route.target_type == "internet_gateway"])
+  nat_gateway_optimize = distinct([for route in var.routes : route.target_name if route.target_type == "nat_gateway"])
 }
 
 data "aws_route_table" "default" {
-  for_each = { for route_rule in var.routes : route_rule.route_table_name => route_rule
-  if local.create_route }
+  for_each = toset(local.route_table_name_optimize)
   tags = {
-    Name = each.value.route_table_name
+    Name = each.key
   }
 }
 
 data "aws_internet_gateway" "default" {
-  for_each = { for route_rule in var.routes : route_rule.target_name => route_rule
-  if local.create_route && route_rule.target_type == "internet_gateway" }
+  for_each = toset(local.internet_gateway_optimize)
   tags = {
-    Name = each.value.target_name
+    Name = each.key
   }
 }
 
 data "aws_nat_gateway" "default" {
-  for_each = { for route_rule in var.routes : route_rule.target_name => route_rule
-  if local.create_route && route_rule.target_type == "nat_gateway" }
+  for_each = toset(local.nat_gateway_optimize)
   tags = {
-    Name = each.value.target_name
+    Name = each.key
   }
 }
